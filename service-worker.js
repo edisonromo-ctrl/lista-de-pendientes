4a8038509052ecbe3df862c6e4297dfdb161b2f5
edisonromo-ctrl <edisonromo@gmail.com>
2026-05-06 18:20:25 -0500

    Renueva cache del logo

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lista-pendientes-v6";
+const CACHE_NAME = "lista-pendientes-v7";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,7 +7,7 @@ const ASSETS = [
   "./app.js",
   "./manifest.webmanifest",
   "./icon.svg",
-  "./hogar-de-larry-logo.png",
+  "./hogar-de-larry-logo.png?v=2",
 ];
 
 self.addEventListener("install", (event) => {
