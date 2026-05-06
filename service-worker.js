49cbc283677783b5be9e0f7f0271360ae93b2f07
edisonromo-ctrl <edisonromo@gmail.com>
2026-05-06 18:53:43 -0500

    Actualiza cache para logo de letras

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lista-pendientes-v9";
+const CACHE_NAME = "lista-pendientes-v10";
 const ASSETS = [
   "./",
   "./index.html",
@@ -7,7 +7,7 @@ const ASSETS = [
   "./app.js",
   "./manifest.webmanifest",
   "./icon.svg",
-  "./hogar-de-larry-logo-v2.jpg",
+  "./hogar-de-larry-logo-letras.jpg",
 ];
 
 self.addEventListener("install", (event) => {
